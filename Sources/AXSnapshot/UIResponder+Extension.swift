6d927ddb3ec45d4622014968f2121aec8dc02f6c
Sungdoo Yoo <dev.esung@gmail.com>
2022-04-02 23:34:35 +0900

    IOS2-1053: add
    IOS2-1053
    
    IOS2-1053

--- Sources/AXSnapshot/UIResponder+Extension.swift
+++ Sources/AXSnapshot/UIResponder+Extension.swift
@@ -1,6 +1,6 @@
 //
 //  UIResponder+Extension.swift
-//  
+//
 //
 //  Created by Sungdoo on 2022/03/12.
 //
@@ -10,7 +10,7 @@ import UIKit
 
 extension UIResponder {
     var isExposedToAssistiveTech: Bool {
-        if isAccessibilityElement {
+        if shouldForceExposeToAssistiveTech || isAccessibilityElement {
             if allItemsInResponderChain.contains(where: { $0.isExposedToAssistiveTech }) == true {
                 return false
             } else {
@@ -19,6 +19,32 @@ extension UIResponder {
         } else {
             return false
         }
+    }
+
+    /// A boolean value indicates that the elment is exposed to AssistiveTechnology even if `isAccessibilityElement` is false
+    ///
+    /// Standard UIKit Controlst are not exposed to AssistiveTechnology when its `hidden` value is true
+    /// - Date: 2022. 04. 02
+    var shouldForceExposeToAssistiveTech: Bool {
+        guard let view = self as? UIView else { return false }
+        return view.isStandardUIKitControl && view.isHidden == false
+    }
+
+    /// A boolean value indicates that the elment is standard UIKit Control
+    ///
+    /// Standard UIKit Controlst are exposed to AssistiveTechnology even if  value of`isAccessibilityElement` is false
+    /// - Date: 2022. 04. 02
+    var isStandardUIKitControl: Bool {
+        (self is UIProgressView) ||
+            (self is UILabel) ||
+            (self is UIButton) ||
+            (self is UISlider) ||
+            (self is UISwitch) ||
+            (self is UITextView) ||
+            (self is UIActivityIndicatorView) ||
+            (self is UIStepper) ||
+            (self is UISegmentedControl) ||
+            (self is UITextField)
     }
 
     var allItemsInResponderChain: [UIResponder] {
