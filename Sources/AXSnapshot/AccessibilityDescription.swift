9874fd4aa15c896b755e03fa414d74edeb1f5ee4
SungdooYoo <dev.esung@gmail.com>
2022-04-02 23:48:39 +0900

    IOS2-1052: generate AccessibilityDescription for Standard UIKit Controls
    @e-sung IOS2-1052: generate AccessibilityDescription for Standard UIKit Controls

--- Sources/AXSnapshot/AccessibilityDescription.swift
+++ Sources/AXSnapshot/AccessibilityDescription.swift
@@ -29,17 +29,52 @@ public var generateAccessibilityDescription: (NSObject) -> String = { object in
 
 public var generateAccessbilityLabelDescription: (NSObject) -> String = { object in
     var description = ""
+
     if let label = object.accessibilityLabel, label.count > 0 {
         description = label
+    } else if let labelText = (object as? UILabel)?.text, labelText.count > 0 {
+        description = labelText
+    } else if let buttonText = (object as? UIButton)?.titleLabel?.text, buttonText.count > 0 {
+        description = buttonText
+    } else if let activityIndicator = object as? UIActivityIndicatorView {
+        description = "Activity Indicator"
+    } else if let stepper = object as? UIStepper {
+        description = "Stepper"
+    } else if let segmentedControl = object as? UISegmentedControl {
+        description = "SegmentedControl with \(segmentedControl.numberOfSegments) segments"
     }
-
     return description
 }
 
 public var generateAccessibilityValueDescription: (NSObject) -> String = { object in
     var description = ""
+
     if let value = object.accessibilityValue, value.count > 0 {
         description = value
+    } else if let progressView = object as? UIProgressView {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .percent
+        let progress = numberFormatter.string(for: progressView.progress) ?? ""
+        description = progress
+    } else if let slider = object as? UISlider {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .percent
+        let percentage = numberFormatter.string(for: slider.value / (slider.maximumValue - slider.minimumValue)) ?? ""
+        description = "Absolute: \(slider.value), Percentage: \(percentage)"
+    } else if let textField = object as? UITextField {
+        description = textField.text ?? ""
+    } else if let textView = object as? UITextView {
+        description = textView.text ?? ""
+    } else if let activityIndicator = object as? UIActivityIndicatorView {
+        description = activityIndicator.isAnimating ? "Animating" : "Halted"
+    } else if let uiSwitch = object as? UISwitch {
+        description = uiSwitch.isOn ? "1" : "0"
+    } else if let segmentedControl = object as? UISegmentedControl {
+        if segmentedControl.selectedSegmentIndex >= 0 {
+            description = "Selected: \(segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex) ?? "")"
+        } else {
+            description = "Selected: None"
+        }
     }
     return description
 }
@@ -48,6 +83,10 @@ public var generateAccessibilityTraitDescription: (NSObject) -> String = { objec
     var description = ""
     if object.accessibilityTraits.isEmpty == false {
         description = "\n\(object.accessibilityTraits.descripion)"
+    } else if object is UIButton || object is UISwitch {
+        description = "\n\(UIAccessibilityTraits.button.descripion)"
+    } else if object is UISlider {
+        description = "\n\(UIAccessibilityTraits.adjustable.descripion)"
     }
     return description
 }
